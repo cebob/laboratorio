a724149f340c591c605dde33c9bbc85053f2ef0f
cebob <54441734+cebob@users.noreply.github.com>
2019-10-22 09:50:55 -0300

    Update vetores.c

--- vetores.c
+++ vetores.c
@@ -27,6 +27,76 @@ float variancia(float * vetor, int n, float med){
 	return soma;
 }
 
+float desvio_padrao(){
+	
+	float * v = NULL;
+	float media = 0, desvio = 0;
+	int n;
+	printf("Digite a quantidade de numeros:");
+	scanf("%d", &n);
+	
+	v = (float *) malloc(sizeof(float) *n);
+	int i;
+	printf("Digite os numeros:");
+	for(i = 0;i<n;i++){
+		scanf("%f", &v[i]);
+	}
+	
+	for(i = 0;i<n;i++){
+		media += v[i];
+	}
+	
+	media = media/n;
+	
+	for(i = 0;i<n;i++){
+		desvio += (v[i] - media)*(v[i] - media);
+	}
+	
+	desvio = desvio/n;
+	desvio = sqrt(desvio);
+	
+	return desvio;
+}
+
+void maior_menor(int n,int * v){
+	int maior = v[0], menor = v[0];
+	int i;
+	for(i=0;i<n;i++){
+		if(maior < v[i]){
+			maior = v[i];
+		}
+		if(menor > v[i]){
+			menor = v[i];
+		}
+	}
+	
+	printf("maior: %d, menor: %d ", maior, menor);
+}
+
+int produto_escalar(int * x, int *  y, int n){
+	int i;
+	int produto = 0;
+	for(i=0;i<n;i++){
+		produto += x[i]*y[i];
+	}
+	
+	return produto;
+	
+}
+
+void ocorrencias(float * x, int n){
+	int i, j, count;
+	float temp;
+	for(i=0;i<n;i++){
+		temp = x[i];
+		for(j=i+1;j<n;j++){
+			if(temp == v[j]){
+				count++;
+			}
+		}
+	}
+}
+
 int main(){
 	int n;
 	printf("Digite a quantidade de numeros: ");
