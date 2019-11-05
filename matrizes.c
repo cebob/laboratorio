f2a7db0b454a064eabbb28b5a6dcde73d9f00a1a
cebob <54441734+cebob@users.noreply.github.com>
2019-11-05 09:42:11 -0300

    Update matrizes.c

--- matrizes.c
+++ matrizes.c
@@ -27,31 +27,54 @@ float** transposta(int m, int n, float **matriz){
 	
 }
 
-void produto(int m, int p, int ** matriz){
+void produto(int m, int p, int n, float ** m1, float ** m2){
 	float ** mat;
 	mat = (float**) malloc(m* sizeof(float*));
 	
-	int i,j;
+	int i,j,k;
 	for(i=0;i<m;i++){
 		mat[i] = (float*) malloc(p*sizeof(float));
 	}
 	
-	// Continuar..		
+	float aux = 0;
+
+	for(i=0;i<m;i++){
+		for(j=0;j<p;j++){
+			for(k=0;k<n;k++){
+				aux+=m1[i][k] * m2[k][j];
+			}
+			mat[i][j] = aux;
+			aux=0;
+		}
+	}
+	
+	for(i=0;i<m;i++){
+		for(j=0;j<p;j++){
+			printf("%f  ", mat[i][j]);
+		}
+		puts("\n");
+	}
 	
 }
 
 int main(){
-	int n,m;
-	scanf("%d %d",&m,&n);
+	int n,m,p;
+	scanf("%d %d %d",&m,&n,&p);
 	
 	float ** mat;
 	mat = (float**) malloc(m* sizeof(float*));
+	float ** mat2;
+	mat2 = (float**) malloc(n* sizeof(float*));
 	
 	int i,j;
 	for(i=0;i<m;i++){
 		mat[i] = (float*) malloc(n*sizeof(float));
 	}
+	for(i=0;i<n;i++){
+		mat2[i] = (float*) malloc(p*sizeof(float));
+	}
 	
+	puts("Matriz 1\n");
 	for(i=0; i<m; i++){
 		for(j=0;j<n;j++){
 			printf("Digite\n");
@@ -59,7 +82,16 @@ int main(){
 		}
 	}
 	
-	transposta(m, n, mat);
+	puts("Matriz 2\n");
+	
+	for(i=0; i<n; i++){
+		for(j=0;j<p;j++){
+			printf("Digite\n");
+			scanf("%f", &mat2[i][j]);
+		}
+	}
+	
+	produto(m, p, n, mat, mat2);
 	
 	return 0;
 }
